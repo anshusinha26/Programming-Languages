84b05e962562e8004c866ffb13476c7e4dabf66a
Anshu Sinha <anshujuly2@gmail.com>
2023-06-01 00:07:27 +0530

    Add a code file on escaping the maze

---
+++ Escaping_the_maze/main.py
@@ -0,0 +1,22 @@
+# Day 6 Project: Reeborg's world
+
+"""
+This is the universal solution for the Reeborg's world's 
+all challenges ranging from 'Alone' to 'Maze challenge'
+"""
+
+# def turn_right():
+#     for i in range(3):
+#         turn_left()
+  
+# while not at_goal():
+#     if (wall_on_right() and front_is_clear()):
+#         move()
+#     elif (wall_on_right() and wall_in_front()):
+#         turn_left()
+#     elif (wall_in_front() and right_is_clear()):
+#         turn_right()
+#         move(
+#     elif (front_is_clear() and right_is_clear()):
+#         turn_right()
+#         move()
