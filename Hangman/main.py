70ecf79fa3f8445874ce21f3c2fee417d80323c4
Anshu Sinha <anshujuly2@gmail.com>
2023-06-01 00:51:16 +0530

    Update main.py of hangman game

--- Hangman/main.py
+++ Hangman/main.py
@@ -1,48 +1,59 @@
-"""imports random module"""
 import random
-"""prints the initial project text"""
-print("Welcome to The Number Guessing Game")
-"""print the initial project text"""
-print("I'm thinking of a number between 1 and 100.")
-"""stores the randomly generated number between 1 and 100, inclusive of 1 and 100"""
-answer = random.randint(1, 100)
-"""prints the hint to the correct answer"""
-print(f"Pssst, the correct answer is {answer}")
-"""asks the user to chose the difficulty level"""
-difficultyLevel = input("Chose a difficulty level. Type 'e' for easy and 'h' for hard: ").lower()
-"""if difficulty level is easy, attempts are set to 10 else attempts are set to 5"""
-if (difficultyLevel == 'e'):
-    attempts = 10
-elif (difficultyLevel == 'h'):
-    attempts = 5
-"""prints the number of attempts a user has"""
-print(f"You have {attempts} attempts remaining to guess the number.")
-"""function definition"""
-def guessTheNumber():
-    """attempts variable is accessed and modified inside this function"""
-    global attempts
-    """asks the user to make a guess"""
-    guess = int(input("Make a guess: "))
-    """if guess is lower than the numerical value of the answer, the 'too low' is printed"""
-    if (guess < answer):
-        print("Too low.")
-        """else if the guess is higher then answer, than 'too high' is printed"""
-    elif (guess > answer):
-        print("Too high.")
-    """unitl attempts are bigger than 1, this loop runs"""
-    while (attempts > 1):
-        """if guess is equal to the answer, the user wins and the loop breaks"""
-        if (guess == answer):
-            print(f"You got it! The answer was {answer}")
-            break
-            """else the user is asked to guess again, attempts are deducted by 1 and the function is called again"""
-        else:
-            print("Guess again.")
-            attempts -= 1
-            print(f"You have {attempts} attempts remaining to guess the number.")
-            guessTheNumber()
-    """if attempts is equal to 1, the user runs out of guesses and he loses"""
-    if (attempts == 1):
-        print("You have run out of guesses, you lose!")
-"""initially the function is called"""
-guessTheNumber()
+
+#TODO-1: - Update the word list to use the 'word_list' from hangman_words.py
+#Delete this line: word_list = ["ardvark", "baboon", "camel"]
+from hangman_words import word_list
+
+chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
+
+end_of_game = False
+lives = 6
+
+#TODO-3: - Import the logo from hangman_art.py and print it at the start of the game.
+from hangman_art import logo
+print(logo)
+
+#Testing code
+print(f'Pssst, the solution is {chosen_word}.')
+
+#Create blanks
+display = []
+for _ in range(word_length):
+    display += "_"
+
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
+
+    #TODO-4: - If the user has entered a letter they've already guessed, print the letter and let them know.
+    if guess in display:
+        print(f"You've already guessed {guess}")
+
+    #Check guessed letter
+    for position in range(word_length):
+        letter = chosen_word[position]
+        #print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
+        if letter == guess:
+            display[position] = letter
+
+    #Check if user is wrong.
+    if guess not in chosen_word:
+        #TODO-5: - If the letter is not in the chosen_word, print out the letter and let them know it's not in the word.
+        print(f"You guessed {guess}, that's not in the word. You lose a life.")
+        
+        lives -= 1
+        if lives == 0:
+            end_of_game = True
+            print("You lose.")
+
+    #Join all the elements in the list and turn it into a String.
+    print(f"{' '.join(display)}")
+
+    #Check if user has got all letters.
+    if "_" not in display:
+        end_of_game = True
+        print("You win.")
+
+    #TODO-2: - Import the stages from hangman_art.py and make this error go away.
+    from hangman_art import stages
+    print(stages[lives])
