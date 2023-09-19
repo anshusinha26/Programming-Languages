52281f447af0247df7b142574a35531ae52e8bbe
Anshu Sinha <anshujuly2@gmail.com>
2023-09-19 19:50:04 +0530

    Add two sample programs

---
+++ C++/A_complete_guide_to_programming_in_C++/2_Fundamental_types_constants_and_variables/sample_programs.cpp
@@ -0,0 +1,32 @@
+// // Sample program 1
+// #include <iostream>
+// #include <climits>       // Definition of INT_MIN, ...
+// using namespace std;
+// int main() {
+//     cout << "Range of types int and unsigned int"
+//         << endl << endl;
+//     cout << "Type             Minimum           Maximum"
+//         << endl
+//         << "--------------------------------------------"
+//         << endl;
+//     cout << "int            " <<  INT_MIN << "        "
+//                                 <<  INT_MAX << endl;
+//     cout << "unsigned int   " <<  "          0        "
+//                                 << UINT_MAX << endl;
+//     return 0; 
+// }
+
+/////
+
+// Sample program 2
+// To display hexadecimal integer literals and
+// decimal integer literals.
+//
+#include <iostream>
+using namespace std;
+int main() {
+    // cout outputs integers as decimal integers:
+    cout << "Value of 0xFF = " << 0xFF << " decimal" << endl; // Output: 255 decimal // The manipulator hex changes output to hexadecimal // format (dec changes to decimal format):
+    cout << "Value of 27 = " << hex << 27 <<" hexadecimal" << endl;              // Output: 1b hexadecimal
+    return 0;
+}
---
+++ C++/A_complete_guide_to_programming_in_C++/2_Fundamental_types_constants_and_variables/tempCodeRunnerFile.cpp
@@ -0,0 +1,6 @@
+    // cout outputs integers as decimal integers:
+    cout << "Value of 0xFF = " << 0xFF << " decimal"
+    << endl; // Output: 255 decimal // The manipulator hex changes output to hexadecimal // format (dec changes to decimal format):
+    cout << "Value of 27 = " << hex << 27 <<" hexadecimal"
+        << endl;              // Output: 1b hexadecimal
+    return 0;
