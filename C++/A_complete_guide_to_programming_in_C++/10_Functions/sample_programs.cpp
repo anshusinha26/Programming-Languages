6126f2aca3df18d422ba867503c76def7e39959d
Anshu Sinha <anshujuly2@gmail.com>
2023-09-24 21:04:23 +0530

    Add a sample program

---
+++ C++/A_complete_guide_to_programming_in_C++/10_Functions/sample_programs.cpp
@@ -0,0 +1,23 @@
+// // Sample program 1
+
+// #include<iostream>
+// #include<iomanip>
+// using namespace std;
+
+// double area(double width, double length) {
+//     return (width * length);
+// }
+
+// int main() {
+//     double width, length;
+//     cin >> width >> length;
+
+//     cout << "The area of a rectangle" << endl;
+//     cout << "with width\t" << fixed << setprecision(2) << width << endl;
+//     cout << "and length\t" << fixed << setprecision(2) << length << endl;
+//     cout << "is\t\t" << setprecision(2) << area(width, length) << endl;
+
+//     return 0;
+// }
+
+/////
