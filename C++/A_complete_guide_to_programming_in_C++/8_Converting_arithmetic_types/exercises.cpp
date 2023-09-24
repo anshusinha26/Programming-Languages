3198e9eeb2be1b3157cd0759c900d9744b83ee66
Anshu Sinha <anshujuly2@gmail.com>
2023-09-24 09:35:15 +0530

    Add solution to the exercises

---
+++ C++/A_complete_guide_to_programming_in_C++/8_Converting_arithmetic_types/exercises.cpp
@@ -0,0 +1,39 @@
+// // Exercise 1
+// // A function has the following prototype
+// // void func( unsigned int n);
+// // What happens when the function is called with –1 as an argument?
+
+// #include<iostream>
+// using namespace std;
+
+// void func(unsigned int n) {
+//     cout << n << endl;
+// }
+
+// int main() {
+//     int n = -1;
+//     func(n);
+
+//     return 0;
+// }
+
+///// 
+
+// // Exercise 2
+// // How often is the following loop executed?
+// // unsigned int limit = 1000;
+// // for (int i = –1; i < limit; i++)
+// // //  . . .
+
+// #include<iostream>
+// using namespace std;
+
+// int main() {
+//     unsigned int limit = 1000;
+//     for (int i = -1; i < limit; i++) {
+//         cout << i << endl;
+//     }
+
+//     return 0;
+// }
+
